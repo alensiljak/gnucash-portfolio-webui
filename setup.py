80299957bd2b37508d0c15e3edbad2ad7fc6fad6
Alen Siljak <alen.siljak@gmx.com>
2018-04-26 12:38:49 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name="gnucash-portfolio-webui",
     packages=["app"],
-    version="0.0.1",
+    version="0.1.0",
     description="Web UI for GnuCash Portfolio",
     author="Alen Siljak",
     author_email="alen.siljak@gmx.com",
